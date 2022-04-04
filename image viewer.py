977116821b55c4bfa1d7644757b9ca202a405190
Omg-A <67595078+Omg-A@users.noreply.github.com>
2022-04-04 20:35:05 +0300

    Add files via upload

--- image viewer.py
+++ image viewer.py
@@ -5,20 +5,31 @@ from PIL import Image, ImageTk
 root = Tk()
 root.title("Image Viewer")
 root.geometry("500x500")
-root.configure(background = "snow")
+root.configure(background = "Black")
 
-label_image = Label(root, bg="gold2", highlightthickness=5, borderwidth=2, relief=SOLID)
+label_image = Label(root)
 label_image.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 img_path = ""
 
 def open_image():
+    global img_path
     img_path = filedialog.askopenfilename(title="Open Image File", filetypes=(("Image Files", ".jpg *.gif *.png *.jpeg"),))
-    print(img_path)
-    img_e = ImageTk.PhotoImage(Image.open(img_path))
+    i = Image.open(img_path)
+    img_e = ImageTk.PhotoImage(i)
     label_image["image"] = img_e
+    img_e.close()
 
-btn = Button(root, font=("sans-serif", 15, "bold"), text="Open Image", bg="gray", fg="white", command=open_image)
+def rotate_image():
+    global img_path
+    i = Image.open(img_path)
+    img_e = ImageTk.PhotoImage(i.rotate(90))
+    label_image["image"] = img_e
+    img_e.close()
+
+btn = Button(root, font=("serif", 13, "normal"), text="Open Image", bg="gray", fg="white", command=open_image)
 btn.place(relx=0.5, rely=0.1, anchor=CENTER)
 
+btn_rotate = Button(root, font=("serif", 13, "normal"), text="Rotate Image", bg="gray", fg="white", command=rotate_image)
+btn_rotate.place(relx=0.5, rely=0.9, anchor=CENTER)
 root.mainloop()
